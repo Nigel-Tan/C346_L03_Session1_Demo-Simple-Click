e9e08f795f556a5c080f718141825ba0d2f2e965
Nigel-Tan <nigel.tan@hotmail.sg>
2023-05-09 11:49:52 +0800

    Finished section e (radiobutton)

--- app/src/main/java/sg/edu/rp/c346/id21023028/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21023028/demosimpleclick/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnDisplay;
     EditText etInput;
     ToggleButton tbtn;
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +28,28 @@ public class MainActivity extends AppCompatActivity {
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
         tbtn = findViewById(R.id.toggleButtonEnabled);
+        rgGender = findViewById(R.id.radioGroupGender);
+
+        rgGender.check(R.id.radioButtonGenderMale); //checks male by default
 
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code for the action
                 String stringResponse = etInput.getText().toString();
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if(checkedRadioId == R.id.radioButtonGenderMale){
+                    // Write the code when male selected
+                    stringResponse = String.format("He says %s",stringResponse);
+                }
+                else{
+                    // Write the code when female selected
+                    stringResponse = String.format("She says %s",stringResponse);
+                }
                 tvDisplay.setText(stringResponse);
-
-
-
             }
         });
+
 
         tbtn.setOnClickListener(new View.OnClickListener() {
             @Override
